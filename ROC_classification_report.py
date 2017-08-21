758bfe16887f7c0b95f7aa66dd9f3c0415a66240
Akhil Garg <31163077+A-Garg@users.noreply.github.com>
2017-08-20 21:24:03 -0400

    Add files, add option to change line style

--- ROC_classification_report.py
+++ ROC_classification_report.py
@@ -17,6 +17,7 @@ Also outputs a classification report, containing precision and recall statistics
 import numpy as np # matrix and math tools, version 1.13.1
 import pandas as pd # data manipulation, version 0.20.2
 import pickle # restore and write serialized data from/to file
+import itertools # to cycle through line styles
 
 # scikit-learn version 0.18.2: tools for machine learning
 from sklearn.metrics import roc_curve, roc_auc_score
@@ -35,10 +36,17 @@ file_list  = ["gridsearch_results_weight_only.pickle",
               "gridsearch_results_age_only.pickle",
               "gridsearch_results_2_features.pickle",
               "gridsearch_results_3_features.pickle",
-              "gridsearch_results_10_features.pickle"]
+              "gridsearch_results_5_features.pickle",
+              "gridsearch_results_8_features.pickle",
+              "gridsearch_results_10_features.pickle",
+              "gridsearch_results_all_features.pickle"]
               
-label_list = ["Weight only (1 feature)", "Age only (1 feature)", 
-              "2 features", "3 features", "10 features"]             
+label_list = ["Weight only", "Age only", 
+              "2 features", "3 features", 
+              "5 features", "8 features", "10 features",
+              "All 74 features"] 
+              
+            
 
 
 print("Plotting results from files:")
@@ -77,7 +85,24 @@ plt.title("ROC of various machine learning models")
 plt.xlabel("False positive rate")
 plt.ylabel("True positive rate")
 
+
+# # Plot what the hypothetical GRACE 2.0 score curve would look like
+# Deprecated because the curve is not ideal--the first part is too steep, and the rest too flat.
+# x = np.linspace(0,1,100)
+# GRACE_AUROC = 0.83
+# # Use a root function as a smooth curve
+# root = 1/(1/(GRACE_AUROC) - 1)
+# y = x ** (1/root)
+# area = np.trapz(y, x)
+# plt.plot(x,y, label = "Hypothetical GRACE 2.0 (area = {:.3f})".format(area))
+
+
 # Plot each ROC curve, corresponding to the best estimator from each gridsearch
+
+# Pick one of the following two lines for plotting (all solid, or alternating)
+line_styles = itertools.cycle(("-"))
+#line_styles = itertools.cycle(("-", "-."))
+
 for i in range(len(file_list)):
 
     X_test = sets[i][1]
@@ -86,7 +111,9 @@ for i in range(len(file_list)):
 
     fpr, tpr, thresholds = roc_curve(y_test, y_score)
     AUROC = roc_auc_score(y_test, y_score)
-    plt.plot(fpr, tpr, label = "{} (area = {:.3f})".format(label_list[i], AUROC))
+    plt.plot(fpr, tpr, 
+             label = "{} (area = {:.3f})".format(label_list[i], AUROC),
+             linestyle = line_styles.next())
 
 # Place legend in bottom right corner (since most action occurs top left)    
 plt.legend(loc = "lower right")
@@ -101,5 +128,5 @@ plt.show()
 
 for i in range(len(file_list)):
     print("Classification report for {}:".format(label_list[i]))
-    print("Which includes features: " + feature_lists[i])
+    print("Which includes features: " + str(feature_lists[i]))
     print(reports[i])
