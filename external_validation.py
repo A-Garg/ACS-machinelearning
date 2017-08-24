767e24facdf0c9211199daf6cc26400561c21c78
Akhil Garg <31163077+A-Garg@users.noreply.github.com>
2017-08-24 14:39:23 -0400

    More user friendly, other fixes
    * Make the script easier to use for a new user
    * Remove some unnecessary imports
    * Better variable names to reduce ambiguity
    * Additional print statements to terminal for AUROC and confidence interval
    * Removed bootstrapping print statements

--- external_validation.py
+++ external_validation.py
@@ -20,11 +20,8 @@ Output: ROCs of mortality predicted by GRACE score vs. machine learning
 import numpy as np # matrix and math tools, version 1.13.1
 import pandas as pd # data manipulation, version 0.20.2
 import pickle # restore and write serialized data from/to file
-import tqdm # monitor progress of for loops, version 4.15.0
-
 
 # scikit-learn version 0.18.2, tools for machine learning
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_curve, roc_auc_score, classification_report
 
 # matplotlib version 2.0.2: tools for plotting
@@ -32,10 +29,55 @@ import matplotlib.pyplot as plt
 
 
 
+''' Variables specific to dataset: make modifications here '''
+
+
+# Enter the file name containing the dataset within the quotes
+file_name = "amiquebec2003.csv"
+
+# Enter the features within the quotes, 
+    # the way they are encoded in the first line of the dataset
+    # The columns must be in order: age, LVEF, peak creatinine, PCI
+feature_columns = ["AGE", "EJECTIONFRACTION", "peakcreat", "pci"]
+
+# Enter the outcome variable, e.g. "death" or "death5yr"
+    # (the way it is encoded in the first line of the dataset)
+response_column = ["death"]
+
+# Enter the column that contains the pre-computed GRACE score
+GRACE_column = ["GRACE"]
+
+
+
+''' Load data '''
+
+
+# Read the dataset to memory
+data = pd.read_csv(file_name, 
+                   usecols    = feature_columns + response_columns + GRACE_column,
+                   low_memory = False,
+                   na_values  = [""," ","ND", "UNKNOWN"])
+
+# Load machine learning model
+with open("classifier3.pickle", "rb") as f:
+    classifier   = pickle.load(f)
+    imputer      = pickle.load(f)
+    standardizer = pickle.load(f)
+
+# Specify the features                
+features = data[feature_columns]
+print("Features: {}".format(list(features)))
+
+# Specify the response
+response = data[str(response_column)]
+print("Response: {}\n".format(response.name))
+
+
+
 ''' Functions '''
 
 
-def AUROC_bootstrap_CI(y_test, y_score, interval = 0.95, n_bootstraps = 10000):
+def AUROC_bootstrap_CI(y_test, y_score, interval = 0.95, n_bootstraps = 1000):
     """
     Calculates the confidence interval for the 
         area under a receiver operating curve statistic.
@@ -46,18 +88,18 @@ def AUROC_bootstrap_CI(y_test, y_score, interval = 0.95, n_bootstraps = 10000):
         y_test:       the true value of the response
         y_score:      the predicted score for the response, given by the classifier
         interval:     the size of the confidence interval, default 0.95 (95%)
-        n_bootstraps: the number of times to resample, default 10000
+        n_bootstraps: the number of times to resample, default 1000
         
     Returns a tuple: (confidence_lower, confidence_upper)
     """
 
-    print("\nCalculating {}% confidence interval.".format(interval*100))
-    print("Bootstrapping with {} random samples.\n".format(n_bootstraps))
+    #print("\nCalculating {}% confidence interval.".format(interval*100))
+    #print("Bootstrapping with {} random samples.\n".format(n_bootstraps))
     
     bootstrapped_scores = []
     rng = np.random.RandomState()
     
-    for i in tqdm.tqdm(range(n_bootstraps)):
+    for i in (range(n_bootstraps)):
         # Bootstrap by sampling with replacement on the prediction indices
         indices = rng.random_integers(0, len(y_score) - 1, len(y_score))
         if len(np.unique(y_test[indices])) < 2:
@@ -78,39 +120,10 @@ def AUROC_bootstrap_CI(y_test, y_score, interval = 0.95, n_bootstraps = 10000):
     
     return (confidence_lower, confidence_upper)
 
-    
 
-''' Load data '''
-
-
-# Load dataset
-data = pd.read_csv("amiquebec2003.csv", 
-                   usecols    = ["AGE","pci", "peakcreat", "GRACE", "death", "death5yr"],
-                   low_memory = False,
-                   na_values  = [""," ","ND", "UNKNOWN"])
-
-# Load machine learning model
-with open("gridsearch_results_3_features.pickle", "rb") as f:
-    classifier      = pickle.load(f).best_estimator_
-    report          = pickle.load(f)
-    train_test_sets = pickle.load(f)
-    feature_list    = pickle.load(f)
-
-# Load preprocessing objects
-with open("preprocessing_3_features.pickle", "rb") as f:
-    imputer      = pickle.load(f)
-    standardizer = pickle.load(f)
-    
-    
     
 ''' Process data and apply the classifier'''
 
-
-# Rearrange columns for the machine learning model                 
-features = data[["peakcreat", "AGE", "pci"]]
-
-# Specify the response
-response = data["death5yr"]
 
 # Drop rows that are missing a response
 available_responses = response.apply(lambda x: x in [0,1])
@@ -139,7 +152,7 @@ prediction_scores = classifier.decision_function(features)
 predictions       = classifier.predict(features)
 
 FPR_ML, TPR_ML, thresholds_ML = roc_curve(response, prediction_scores)
-AUROC = roc_auc_score(response, prediction_scores)
+ML_AUROC = roc_auc_score(response, prediction_scores)
 ML_CI_low, ML_CI_high = AUROC_bootstrap_CI(response, prediction_scores)
 
 #print(classification_report(response, predictions))
@@ -158,17 +171,21 @@ plt.plot([0, 1], [0, 1],
 
 # Plot the machine learning ROC
 plt.plot(FPR_ML, TPR_ML, 
-         label = "Machine learning (area = {:.3f}, CI = [{:0.3f} - {:0.3}])".format(
-            AUROC, ML_CI_low, ML_CI_high))
+         label = "Machine learning (area = {:.3f}, CI = [{:0.3f} - {:0.3f}])".format(
+            ML_AUROC, ML_CI_low, ML_CI_high))
+print("Machine learning AUROC: {:.3f} [{:0.3f} - {:0.3f}]".format(
+      ML_AUROC, ML_CI_low, ML_CI_high))
 
 # Plot the GRACE ROC
-AUROC = roc_auc_score(response, data["GRACE"])
+GRACE_AUROC = roc_auc_score(response, data["GRACE"])
 FPR_GRACE, TPR_GRACE, thresholds_GRACE = roc_curve(response, data["GRACE"])
 GRACE_CI_low, GRACE_CI_high = AUROC_bootstrap_CI(response, data["GRACE"])
 
 plt.plot(FPR_GRACE, TPR_GRACE, 
-         label = "GRACE (area = {:.3f}, CI = [{:0.3f} - {:0.3}])".format(
-            AUROC, GRACE_CI_low, GRACE_CI_high))
+         label = "GRACE (area = {:.3f}, CI = [{:0.3f} - {:0.3f}])".format(
+            GRACE_AUROC, GRACE_CI_low, GRACE_CI_high))
+print("GRACE AUROC: {:.3f} [{:0.3f} - {:0.3f}]".format(
+      GRACE_AUROC, GRACE_CI_low, GRACE_CI_high))
             
 # Set plot parameters
 plt.axis('scaled') # force 1:1 aspect ratio
