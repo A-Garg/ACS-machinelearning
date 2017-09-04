2877b3a24ab2c23b940952a15349e0727fe34e79
Akhil Garg <31163077+A-Garg@users.noreply.github.com>
2017-09-04 11:38:34 -0400

    Delete hello.py

--- Website/hello.py
+++
@@ -1,7 +0,0 @@
-print 'Content-Type: text/html'
-print
-print '<html>'
-print '<head><title>Hello from Python</title></head>'
-print '<body>'
-print '<h2>Hello from Python</h2>'
-print '</body></html>'
